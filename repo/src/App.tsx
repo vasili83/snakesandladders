af3b2f6ff06fb2ba88ab6454f6617bd170d01562
Vasili Paspalas <gitkraken@vasili.nl>
2026-02-08 14:44:14 +0100

    ladder-snake message
    didnt add message for snake or ladder. Also added some visual improvements

--- repo/src/App.tsx
+++ repo/src/App.tsx
@@ -2,22 +2,22 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const diceSymb:string[] = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
-  const ladders:[number, number][] = [[15,25], [3,16], [21,32]];
-  const snakes:[number, number][] = [[2,12], [4,30], [22,35]];
+  const diceSymb: string[] = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+  const ladders: [number, number][] = [[15, 25], [3, 16], [21, 32]];
+  const snakes: [number, number][] = [[2, 12], [4, 30], [22, 35]];
 
   let [dice1, setDice1] = useState<number>(0);
   let [dice2, setDice2] = useState<number>(0);
   let [diceSum, setDiceSum] = useState<number>(0);
   let [playerpos, setPlayerpos] = useState<number>(1);
-  let [winlosemssg, setWinlosemssg] = useState<string>("");
+  let [winlosemssg, setWinlosemssg] = useState<string>(" ");
 
-  useEffect(()=>{
-      youWin(playerpos) && setWinlosemssg("Je bent precies op vak 36 gekomen!");
-      youLose(playerpos) && setWinlosemssg("Helaas verloren; vanaf vak 35 kun je nooit meer op 36 komen.. refresh je pagina");
+  useEffect(() => {
+    youWin(playerpos) && setWinlosemssg("Gewonnen! Je bent precies op vak 36 gekomen!");
+    youLose(playerpos) && setWinlosemssg("Helaas verloren: vanaf vak 35 kun je nooit meer op 36 komen.. refresh je pagina");
   }, [playerpos]);
 
-  const throwDice = ()=>{
+  const throwDice = () => {
     const newDice1 = Math.floor(Math.random() * 6) + 1;
     const newDice2 = Math.floor(Math.random() * 6) + 1;
     const newSum = newDice1 + newDice2;
@@ -29,31 +29,42 @@ function App() {
   }
 
 
-  const checkLaddersOrSnakes = (rolledDiceSum:number):number=>{
+  const checkLaddersOrSnakes = (rolledDiceSum: number): number => {
     let prospectPos = rolledDiceSum + playerpos;
 
-    const moveUp = (): number | undefined => {    
-     let arrPair:[number, number] | undefined = ladders.find((entry) => Math.min(...entry) === prospectPos);
-     return arrPair ? Math.max(...arrPair) : undefined; 
+    const moveUp = (): number | undefined => {
+      let arrPair: [number, number] | undefined = ladders.find((entry) => Math.min(...entry) === prospectPos);
+      if (arrPair) {
+        setWinlosemssg("Super! Je hebt een ladder genomen");
+        return Math.max(...arrPair)
+      } else {
+        return undefined;
+      }
     }
 
-    const moveDown = (): number | undefined => {    
-     let arrPair:[number, number] | undefined = snakes.find((entry) => Math.max(...entry) === prospectPos);
-     return arrPair ? Math.min(...arrPair) : undefined; 
+    const moveDown = (): number | undefined => {
+      let arrPair: [number, number] | undefined = snakes.find((entry) => Math.max(...entry) === prospectPos);
+      if (arrPair) {
+        setWinlosemssg("Jammer, een slang die je stappen terug zet");
+        return Math.min(...arrPair);
+      } else { return undefined; }
     }
 
     return moveUp() || moveDown() || prospectPos;
   }
 
-  const movePlayer = (rolledDiceSum:number)=>{
+  const movePlayer = (rolledDiceSum: number) => {
+    setWinlosemssg(" ");
     const newPos = checkLaddersOrSnakes(rolledDiceSum);
-    if(newPos < 37){
+    if (newPos < 37) {
       setPlayerpos(newPos);
+    } else {
+      setWinlosemssg('Overshoot! Je blijft staan op veld staan');
     }
   }
 
-  let youWin = (pos:number):boolean => pos === 36;
-  let youLose = (pos:number):boolean => pos === 35;
+  let youWin = (pos: number): boolean => pos === 36;
+  let youLose = (pos: number): boolean => pos === 35;
 
   return (
     <>
@@ -61,10 +72,10 @@ function App() {
         <h1>Snake & Ladders</h1>
       </header>
       <main>
-        <h2>{winlosemssg}</h2>
+        <h2 className="mssg">{winlosemssg}</h2>
         <h3>Speler positie: {playerpos}</h3>
-        <p>Dobbelsteen: {dice1!==0 && diceSymb[dice1-1]} | Dobbelsteen: {dice2!==0 && diceSymb[dice2-1]}</p>
-        <p>{diceSum}</p>
+        <p>Dobbelsteen: <span className="dice">{dice1 !== 0 && diceSymb[dice1 - 1]}</span> | Dobbelsteen: <span className="dice">{dice2 !== 0 && diceSymb[dice2 - 1]}</span></p>
+        <p>Totaal dobbelstenen: {diceSum}</p>
         <button onClick={throwDice}>Gooi!</button>
       </main>
     </>
